ff4fa60fb766c6f207b85c64913687f7e20b3a2c
Gonzalo <gonalba@ucm.es>
2021-11-07 18:05:40 +0100

    Estructura basica para trabajar con el motor

--- Launcher/pc/src/main/java/es/ucm/vdm/launcher/pc/PCLauncher.java
+++ Launcher/pc/src/main/java/es/ucm/vdm/launcher/pc/PCLauncher.java
@@ -1,11 +1,21 @@
 package es.ucm.vdm.launcher.pc;
 
+import es.ucm.vdm.engine.pc.PCEngine;
 import es.ucm.vdm.logic.Board;
+import es.ucm.vdm.logic.Logic;
 
 public class PCLauncher {
     public static void main(String[] args) {
-        Board b = new Board();
-        b.setBoard(5);
+
+        Logic l = new Logic();
+        PCEngine e = new PCEngine();
+        e.createWindow("0h n0", 600, 400);
+        e.setState(l);
+        e.run();
+
+
+//        Board b = new Board();
+//        b.setBoard(5);
 
 //        b.getTile(1,2).change();
 //        b.getTile(0,0).change();
--- Logic/src/main/java/es/ucm/vdm/logic/Logic.java
+++ Logic/src/main/java/es/ucm/vdm/logic/Logic.java
@@ -1,5 +1,23 @@
 package es.ucm.vdm.logic;
 
-public class Logic {
+import es.ucm.vdm.engine.common.Engine;
+import es.ucm.vdm.engine.common.Graphics;
+import es.ucm.vdm.engine.common.State;
 
+public class Logic implements State {
+
+    @Override
+    public boolean init(Engine engine) {
+        return true;
+    }
+
+    @Override
+    public void update(double deltaTime) {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
 }
